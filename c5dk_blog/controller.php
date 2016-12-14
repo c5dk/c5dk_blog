db9e53eba40e550e970e52733a6c199d5ef1ff37
Ronny Jespersen <bitbucket.org@rjj-net.dk>
2016-12-14 14:17:55 +0700

    v8.0.6.1 - Fixed issue with config values overriding old values under upgrade.

--- c5dk_blog/controller.php
+++ c5dk_blog/controller.php
@@ -18,7 +18,7 @@ defined('C5_EXECUTE') or die("Access Denied.");
 class Controller extends Package {
 
 	protected $appVersionRequired		= '8.0.1';
-	protected $pkgVersion				= '8.0.6';
+	protected $pkgVersion				= '8.0.6.1';
 	protected $pkgHandle				= 'c5dk_blog';
 	protected $pkgAutoloaderRegistries	= array(
 		'src/C5dkBlog' => '\C5dk\Blog'

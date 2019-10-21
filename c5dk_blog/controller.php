9fc296ba533f18c501be1126f1d3ba0de9542fba
Ronny Jespersen <bitbucket.org@rjj-net.dk>
2019-10-21 23:33:27 +0700

    Chg: Removed exclude_subpages_from_nav This attribute don't exist in empty site install

--- c5dk_blog/controller.php
+++ c5dk_blog/controller.php
@@ -211,7 +211,7 @@ class Controller extends Package
 		// Normal
 		$singlePage = C5dkInstaller::installSinglePage('/blog_post', t('Blog Post'), t('Add/Edit a blog post'), $pkg, ['exclude_nav' => 1]);
 
-		$singlePage = C5dkInstaller::installSinglePage('/c5dk', t('C5DK'), t("Shared folder for C5DK Packages"), $pkg, ['exclude_nav' => 1, 'exclude_page_list' => 1, 'exclude_search_index' => 1, 'exclude_subpages_from_nav' => 1]);
+		$singlePage = C5dkInstaller::installSinglePage('/c5dk', t('C5DK'), t("Shared folder for C5DK Packages"), $pkg, ['exclude_nav' => 1, 'exclude_page_list' => 1, 'exclude_search_index' => 1]);
 		// $singlePage = C5dkInstaller::installSinglePage('/c5dk/blog', t('Blog'), t("Main folder for the C5DK Blog package"), $pkg, ['exclude_nav' => 1, 'exclude_page_list' => 1]);
 		// $singlePage = C5dkInstaller::installSinglePage('/c5dk/blog/editor', t('Editor'), t("Blog Editor"), $pkg, ['exclude_nav' => 1, 'exclude_page_list' => 1]);
 		$singlePage = C5dkInstaller::installSinglePage('/c5dk/blog/editor/manager', t('Manager'), t("Blog Editor Manager"), $pkg, ['exclude_nav' => 1, 'exclude_page_list' => 1]);

6bccd6301b7ca513c77ed0a6a7142c1575b4b72f
Ronny Jespersen <bitbucket.org@rjj-net.dk>
2019-11-08 18:54:48 +0700

    Chg: Removed cancel button on settings

--- c5dk_blog/single_pages/dashboard/c5dk_blog/blog_settings.php
+++ c5dk_blog/single_pages/dashboard/c5dk_blog/blog_settings.php
@@ -155,7 +155,7 @@
 	<div class="ccm-dashboard-form-actions-wrapper">
 		<div class="ccm-dashboard-form-actions">
 			<?= $form->submit('submit', t('Save'), [], 'pull-right btn btn-success'); ?>
-			<a class="btn ccm-input-submit pull-right btn btn-danger" href="<?= URL::to('/dashboard/c5dk_blog/blog_roots/'); ?>"><?= t('Cancel'); ?></a>
+			<!-- <a class="btn ccm-input-submit pull-right btn btn-danger" href="<?= URL::to('/dashboard/c5dk_blog/blog_roots/'); ?>"><?= t('Cancel'); ?></a> -->
 		</div>
 	</div>
 

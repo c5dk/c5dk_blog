87fbca6bd5e72e9b7e830bf444d7aa27214cdf53
Ronny Jespersen <github@rjj-net.dk>
2020-02-16 20:12:59 +0700

    Fix: Added langpath on link to root

--- c5dk_blog/single_pages/c5dk/blog/editor/manager/view.php
+++ c5dk_blog/single_pages/c5dk/blog/editor/manager/view.php
@@ -16,7 +16,7 @@
 				<td><?= $C5dkRoot->getCollectionName(); ?></td>
 				<td><a href="<?= $C5dkRoot->getCollectionLink(); ?>"><?= $C5dkRoot->getCollectionPath(); ?></a></td>
 				<td>
-					<a class="btn btn-primary btn-sm" href="<?= URL::to('c5dk/blog/editor/manager/root', $C5dkRoot->getCollectionID()); ?>">
+					<a class="btn btn-primary btn-sm" href="<?= URL::to($langpath . '/c5dk/blog/editor/manager/root', $C5dkRoot->getCollectionID()); ?>">
 						<?= t('Manage Root'); ?>
 					</a>
 				</td>

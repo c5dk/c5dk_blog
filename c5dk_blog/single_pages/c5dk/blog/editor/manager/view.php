3bc6b7fc43eef7bd4d7be29a26a557ac50cc98e1
Ronny Jespersen <github@rjj-net.dk>
2020-02-27 20:23:37 +0700

    Chg: Merge from Michael

--- c5dk_blog/single_pages/c5dk/blog/editor/manager/view.php
+++ c5dk_blog/single_pages/c5dk/blog/editor/manager/view.php
@@ -8,14 +8,14 @@
 		<tr class="c5dk_blog_package_header">
 			<th class="left_round_corner"><?= t('Root'); ?></th>
 			<th><?= t('Path'); ?></th>
-			<th><?= t('Actions'); ?></th>
+			<th style="text-align:right;"><?= t('Actions'); ?></th>
 		</tr>
 
 		<?php foreach ($rootList as $rootID => $C5dkRoot) { ?>
 			<tr class="c5dk_blog_package_root_header">
 				<td><?= $C5dkRoot->getCollectionName(); ?></td>
 				<td><a href="<?= $C5dkRoot->getCollectionLink(); ?>"><?= $C5dkRoot->getCollectionPath(); ?></a></td>
-				<td>
+				<td style="text-align:right;">
 					<a class="btn btn-primary btn-sm" href="<?= URL::to($langpath . '/c5dk/blog/editor/manager/root', $C5dkRoot->getCollectionID()); ?>">
 						<?= t('Manage Root'); ?>
 					</a>
